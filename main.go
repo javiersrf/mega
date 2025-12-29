3d72ec84ba93887db87ec2ab1ae61403a7c538d7
javiersrf <javiersrf@gmail.com>
2025-12-29 15:18:18 -0300

    feat: update docs and cors

--- main.go
+++ main.go
@@ -10,18 +10,40 @@ import (
 	ginSwagger "github.com/swaggo/gin-swagger"
 )
 
+func CORSMiddleware() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
+		c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
+		c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
+		c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, OPTIONS, GET, PUT")
+
+		if c.Request.Method == "OPTIONS" {
+			c.AbortWithStatus(204)
+			return
+		}
+
+		c.Next()
+	}
+}
+
 func main() {
 	port := flag.String("port", "8080", "Port to run the server on")
 	flag.Parse()
 
 	r := gin.Default()
+	r.Use(CORSMiddleware())
 	docs.SwaggerInfo.BasePath = "/api/v1"
 
 	r.GET("/health", handlers.HealthCheckHandler)
-	r.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerfiles.Handler))
-	v1 := r.Group("/api/v1")
+
+	api := r.Group("/api")
+
 	{
-		v1.POST("/megasena/calculate", handlers.CalculateHandler)
+		api.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerfiles.Handler))
+		v1 := api.Group("/v1")
+		{
+			v1.POST("/megasena/calculate", handlers.CalculateHandler)
+		}
 	}
 
 	r.Run(":" + *port)
